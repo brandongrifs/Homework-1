6d5b638e2158f64a84bb05a494ec3fa61780a665
Brandon Griffin <brandongrifs@berkeley.edu>
2017-09-15 09:53:27 -0700

    homework one trial 1

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,10 +4,12 @@ contract greeter {
 	/* Add one variable to hold our greeting */
 	string[4] greeting = ["Hello World", "Hey Dude", "What's Poppin'", "Yooooo"];
 	uint count=0;
+	uint calls=0;
 
 	function greeter(string _greeting) public {
-	    bytes memory greets = bytes(_greeting);
-	    count = greets.length%3;
+		bytes memory greets = bytes(_greeting);
+		count = greets.length%3;
+		calls = calls +1;
 	}
 
 	function greet() constant returns (string)  {
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,9 +2,32 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+	event  fibonacciFound(uint position, uint result);
+
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+
+		uint n1=1;
+		uint n2=1;
+		if(position==0)
+		return n1;
+		else if(position==1)
+		return n2;
+
+		uint count=2;
+		uint newResult = n1+n2;
+		n1=n2;
+		n2=newResult;
+		while(count<position){
+			count++;
+			newResult = n2+n1;
+			n1=n2;
+			n2=newResult;
+		}
+		fibonacciFound(position, newResult);
+		return newResult;
 	}
 
+
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() {require(msg.value==0);}
 }
